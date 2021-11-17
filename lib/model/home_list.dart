ca6da33413cc6dfbd8a6e5ace18cdf987bc807f8
CaicoLeung <lkk1053190902@gmail.com>
2021-11-18 00:17:51 +0800

    feat: model home_list

---
+++ lib/model/home_list.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:startup_namer/design_course/design_course_screen.dart';
+import 'package:startup_namer/fitness_app/fitness_app_screen.dart';
+import 'package:startup_namer/hotel_booking/hotel_booking_screen.dart';
+import 'package:startup_namer/introduction_animation/introduction_animation_screen.dart';
+
+class HomeList {
+  HomeList({this.navigateScreen, this.imagePath = ""});
+
+  Widget? navigateScreen;
+  String imagePath;
+
+  static List<HomeList> homeList = [
+    HomeList(
+        imagePath: 'assets/introduction_animation/introduction_animation.png',
+        navigateScreen: const IntroductionAnimationScreen()
+    ),
+    HomeList(
+        imagePath: 'assets/hotel/hotel_booking.png',
+        navigateScreen: const HotelBookingScreen()
+    ),
+    HomeList(
+        imagePath: 'assets/fitness_app/fitness_app.png',
+        navigateScreen: const FitnessAppScreen()
+    ),
+    HomeList(
+        imagePath: 'assets/design_course/design_course.png',
+        navigateScreen: const DesignCourseScreen()
+    ),
+  ];
+}
