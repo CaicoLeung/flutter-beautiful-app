6704f7961df4181b518e71690246afa0924e43b5
CaicoLeung <lkk1053190902@gmail.com>
2021-11-17 16:50:00 +0800

    feat: NavigationHomeScreen

--- lib/navigation_home_screen.dart
+++ lib/navigation_home_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:startup_namer/app_theme.dart';
+import 'app_theme.dart';
+import 'custom_drawer/drawer_user_controller.dart';
+
+import 'custom_drawer/helper.dart';
+import 'home_screen.dart';
 
 class NavigationHomeScreen extends StatefulWidget {
   const NavigationHomeScreen({Key? key}) : super(key: key);
@@ -9,16 +13,35 @@ class NavigationHomeScreen extends StatefulWidget {
 }
 
 class _NavigationHomeScreenState extends State<NavigationHomeScreen> {
+  Widget? screenView;
+  DrawerIndex? drawerIndex;
+
+  @override
+  void initState() {
+    screenView = const HomeScreen();
+    drawerIndex = DrawerIndex.home;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       color: AppTheme.nearlyWhite,
-      child: const SafeArea(
+      child:  SafeArea(
+        top: false,
+        bottom: false,
         child: Scaffold(
-          body: Text("TODO", style: TextStyle(fontStyle: FontStyle.italic),),
+          backgroundColor: AppTheme.nearlyWhite,
+          body: DrawerUserController(
+            screenIndex: drawerIndex!,
+            drawerWidth: MediaQuery.of(context).size.width * 0.75,
+            onDrawerCall: (index) {
+              // TODO
+            },
+            screenView: screenView,
+          ),
         ),
       ),
     );
   }
-
 }
